8eaa8ed35533f1f0412b4846144d7c0c90f80df7
TessKi <kinnte@gmail.com>
2018-11-16 14:12:45 +0000

    added packages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const env = require('./config/environment');
 const mongoose = require('mongoose');
 mongoose.connect(env.dbUri);
 
-const router = require('./config/router');
+// const router = require('./config/router');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -18,7 +18,7 @@ console.log(env.port, 'is port');
 app.use(express.static(`${__dirname}/public`));
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.use('/api', router);
+// app.use('/api', router);
 
 app.listen(env.port, () => console.log(`Express is running on port ${env.port}`));
 
