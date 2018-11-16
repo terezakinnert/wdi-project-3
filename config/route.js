f99bd743cdc69724d2c6fdccc5cccbc82410190d
TessKi <kinnte@gmail.com>
2018-11-16 14:28:06 +0000

    register and login route

--- config/route.js
+++ config/route.js
@@ -1,3 +1,29 @@
 const router = require('express').Router();
+// const env = require('./environment');
+// const jwt = require('jsonwebtoken');
+
+const authCtrl = require('../controllers/authCtrl');
+
+// function secureRoute(req, res, next) {
+//   if(!req.headers.authorization)
+//     res.status(401).json({ message: 'Unauthorised user'});
+//   const token = req.headers.authorization.replace('Bearer ', '');
+//   jwt.verify(token, env.secret, function(err) {
+//     if (err) {
+//       res.status(401).json({ message: 'Unauthorised user'});
+//     } else {
+//       req.tokenUserId = jwt.decode(token).sub;
+//       next();
+//     }
+//   });
+// }
+
+router.route('/register')
+  .post(authCtrl.register);
+
+router.route('/login')
+  .post(authCtrl.login);
+
+
 
 module.exports = router;
