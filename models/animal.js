b384229c574257eacee9903746adf8c7912bbad2
TessKi <kinnte@gmail.com>
2018-11-16 13:36:10 +0000

    created an animal model

---
+++ models/animal.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const animalSchema = mongoose.Schema({
+  name: String,
+  bio: String,
+  sex: String,
+  species: String,
+  breed: String,
+  age: Number,
+  color: [String],
+  imageUrl: String,
+  location: String,
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  rating: [Number],
+  availableOn: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Booking'
+  },
+  comments: [{
+    text: String,
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  }]
+});
+
+const animalModel = mongoose.model('Animal', animalSchema);
+module.exports = animalModel;
