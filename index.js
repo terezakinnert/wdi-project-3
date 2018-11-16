b3407b78f1778bc5ff65185d712c82bf5beab314
TessKi <kinnte@gmail.com>
2018-11-16 13:20:50 +0000

    set up port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const { port } = require('./config/environment');
+const port = require('./config/environment');
+console.log(port, 'is port');
 
 app.use(express.static(`${__dirname}/public`));
 
