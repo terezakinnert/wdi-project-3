42bcec1cda996421b8306d8fb39e9070d35d2a05
TessKi <kinnte@gmail.com>
2018-11-19 16:27:06 +0000

    index ctrl edit

--- src/controllers/animals/indexController.js
+++ src/controllers/animals/indexController.js
@@ -1,12 +1,11 @@
 function indexController($scope, $http) {
   $http({
     method: 'GET',
-    url: '/api/animals'
+    url: '/api/pets'
   }).then(result => {
     $scope.allAnimals = result.data;
     $scope.filteredAnimals = $scope.allAnimals;
   });
-
   $scope.handleFilterSubmit = function() {
     $scope.filteredAnimals = $scope.allAnimals.filter(animal =>
       animal.name.toLowerCase().startsWith($scope.searchTerm.toLowerCase())
@@ -15,3 +14,24 @@ function indexController($scope, $http) {
 }
 
 export default indexController;
+//
+//
+// function indexCtrl($scope, $http, $auth) {
+//   $http({
+//     method: 'GET',
+//     url: '/api/fishes'
+//   }).then(result => {
+//     $scope.allFishes = result.data;
+//     $scope.filteredFishes = $scope.allFishes;
+//   });
+//
+//   $scope.handleFilterSubmit = function() {
+//     console.log('Filter form submitted!', $scope.searchTerm);
+//     $scope.filteredFishes = $scope.allFishes.filter(fish =>
+//       fish.name.toLowerCase().startsWith($scope.searchTerm.toLowerCase())
+//     );
+//   };
+// }
+//
+// export default indexCtrl;
+//
