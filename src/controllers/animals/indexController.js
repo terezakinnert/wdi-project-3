ffce42ba6a0cbd4815392ba4cc227a2e4d8eaced
TessKi <kinnte@gmail.com>
2018-11-19 16:37:16 +0000

    index again

--- src/controllers/animals/indexController.js
+++ src/controllers/animals/indexController.js
@@ -14,24 +14,3 @@ function indexController($scope, $http) {
 }
 
 export default indexController;
-//
-//
-// function indexCtrl($scope, $http, $auth) {
-//   $http({
-//     method: 'GET',
-//     url: '/api/fishes'
-//   }).then(result => {
-//     $scope.allFishes = result.data;
-//     $scope.filteredFishes = $scope.allFishes;
-//   });
-//
-//   $scope.handleFilterSubmit = function() {
-//     console.log('Filter form submitted!', $scope.searchTerm);
-//     $scope.filteredFishes = $scope.allFishes.filter(fish =>
-//       fish.name.toLowerCase().startsWith($scope.searchTerm.toLowerCase())
-//     );
-//   };
-// }
-//
-// export default indexCtrl;
-//
